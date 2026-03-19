c2beefda0e099ae304a6651a289a13e76f064574
DiegoNoguez <noguezlopezarturo@gmail.com>
2026-03-19 08:34:55 -0600

    Se actualiza a liga de pruebas por tunnel

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // Archivo para configurar las Apis
 // LIga de desarrollo 
-//const API_URL = "http://127.0.0.1:8000/api/";
+//cdcdconst API_URL = "http://127.0.0.1:8000/api/";
 
 // liga de PRUEBA EN DEPLOYS
-const API_URL = "https://leavenless-natasha-isocheimenal.ngrok-free.dev"
+const API_URL = "https://leavenless-natasha-isocheimenal.ngrok-free.dev/api/"
